9784665ae5e0b524b18b7c6ccf4d04540a262015
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-29 16:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+ window.addEventListener('load', () => {
+            // DOM load success  
+            const body = document.getElementsByTagName('body')
+            body.textContent = "DOM load success";
+        })
